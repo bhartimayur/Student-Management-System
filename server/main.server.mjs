ea2b7fe16401cbb67083153f5f171fe937e9fcdc
bhartimayur <bhartimayur815@gmail.com>
2023-12-30 23:50:26 +0530

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-TXXTJHFN.mjs";import"./chunk-7ZOKYJUZ.mjs";import"./chunk-V33DDLTH.mjs";import"./chunk-3SZVCSKE.mjs";import"./chunk-LK223424.mjs";export{a as default};
